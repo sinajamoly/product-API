b8dd8e2996c5b733418e4f6bea137e30a23ed9c1
sinajamoly <sinajamoly@gmail.com>
2018-06-03 16:04:25 -0700

    adding routes and body parser middleware

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 //ROUTES----------------------------------------------------------------
 const productRoutes = require('./api/routes/products');
@@ -15,7 +27,7 @@ app.use('/orders', orderRoutes);
 
 app.use((req, res, next)=>{
     const error = new Error('Not Found');
-    error.status(404);
+    error.status = 400;
     next(error);
 })
 
