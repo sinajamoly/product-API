61d23eef8ca194dab4e13f7cd8e54fdabe29cdfd
sinajamoly <sinajamoly@gmail.com>
2018-06-08 15:33:26 -0700

    sign in

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next)=>{
     User.find({email: req.body.email})
@@ -40,6 +41,50 @@ router.post('/signup', (req, res, next)=>{
         }
     })
 
+})
+
+router.post('/login',(req, res, next)=>{
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if(user.length < 1){
+                return res.status(401).json({
+                    message: 'Auth failed'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+                if(err){
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                }
+                if(result){
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                        },
+                        'secrett',
+                        {
+                            expiresIn: "1h",
+
+                        }
+                    );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                }
+
+                res.status(401).json({
+                    message: 'Auth failed'
+                })
+            })
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error: err
+            });
+        });
 })
 
 router.delete('/:userId', (req, res, next)=>{
